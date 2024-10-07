96eba31d8460cceb32f6dfcf8b34a9b5f4ca5f10
KatarzynaAfrontech <kzalobafrontend@gmail.com>
2024-10-07 07:54:25 +0200

    add StyledButton to the List component

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -1,17 +1,36 @@
-import { Item } from "../Item/Item.jsx";
+import {Item} from "../Item/Item.jsx";
 import styles from "./List.module.css";
+import styled from "styled-components";
 
 const emojis = ["🚀", "🎶", "🎁", "🎉", "✨"];
+
+const StyledButton = styled.button`
+  background: transparent;
+  border: solid 1px;
+  padding: 12px;
+  border-radius: 5px;
+  cursor: pointer;
+  width: 100px;
+  color: salmon;
+  transition: background 0.3s;
+  margin: 30px;
+
+  &:hover {
+    color: white;
+    background: salmon;
+  }
+`;
 
 export function List() {
     return (
         <>
+            <StyledButton>Test</StyledButton>
             <button className={styles.btn} onClick={() => alert(`Liczba emoji: ${emojis.length}`)}>
-            Pokaż liczbę emoji
+                Pokaż liczbę emoji
             </button>
             <ul className={styles.list}>
                 {emojis.map((emoji) => (
-                    <Item key={emoji} emoji={emoji} />
+                    <Item key={emoji} emoji={emoji}/>
                 ))}
             </ul>
         </>
