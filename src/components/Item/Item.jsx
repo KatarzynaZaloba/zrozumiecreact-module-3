6dcd218031e39d28754126b218928e31ad74bfb4
KatarzynaAfrontech <kzalobafrontend@gmail.com>
2024-10-08 08:01:27 +0200

    Add StyledItem and StyledEmoji styled components

--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -1,12 +1,28 @@
 import { useState } from "react";
 import styles from "./Item.module.css";
+import styled from "styled-components";
+
+const StyledItem = styled.li`
+  background: salmon;
+  padding: 30px 50px;
+  border-radius: 20px;
+  text-align: center;
+  overflow: hidden;
+`;
+
+const StyledEmoji = styled.span`
+  display: block;
+  margin-bottom: 42px;
+  font-size: 32px;
+  transition: transform 0.3s;
+`;
 
 export function Item({ emoji }) {
     const [zoomed, setZoomed] = useState(false);
 
     return (
-        <li className={styles.item}>
-            <span className={`${styles.emoji} ${zoomed ? styles.zoomed : ''}`}>{emoji}</span>
+        <StyledItem>
+            <StyledEmoji>{emoji}</StyledEmoji>
             <button
                 className={styles.btn}
                 onClick={() => {
@@ -15,6 +31,6 @@ export function Item({ emoji }) {
             >
                 {zoomed ? "Oddal" : "Przybliż"}
             </button>
-        </li>
+        </StyledItem>
     );
 }
